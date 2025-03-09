51fc78d785d6c4c21483bbc88b25bce09671d650
NaudelynLucena <naulunau@gmail.com>
2025-03-09 23:41:54 +0100

    feat: implement full DNA to RNA transcription. Refactore code, a  switch-case is used to cover all nucleotides, test passes

--- src/main/java/dev/nau/rna_transcription/RnaTranscription.java
+++ src/main/java/dev/nau/rna_transcription/RnaTranscription.java
@@ -2,8 +2,27 @@ package dev.nau.rna_transcription;
 
 public class RnaTranscription {
     public static String toRNA(String dna) {
-        if (dna.equals("C")) return "G";
-        return "";
-    }
+        StringBuilder rna = new StringBuilder();
 
+        for (char nucleotide : dna.toCharArray()) {
+            switch (nucleotide) {
+                case 'C':
+                    rna.append('G');
+                    break;
+                case 'G':
+                    rna.append('C');
+                    break;
+                case 'T':
+                    rna.append('A');
+                    break;
+                case 'A':
+                    rna.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid nucleotide in DNA sequence: " + nucleotide);
+            }
+        }
+
+        return rna.toString();
+    }
 }
