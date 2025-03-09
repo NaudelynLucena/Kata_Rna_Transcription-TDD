88682aba20cbc34b7b8b110e9a082e2bf868bfc1
NaudelynLucena <naulunau@gmail.com>
2025-03-09 22:38:50 +0100

    test: add test for cytosine to guanine transcription, test fails

--- src/test/java/dev/nau/rna_transcription/RnaTranscriptionTest.java
+++ src/test/java/dev/nau/rna_transcription/RnaTranscriptionTest.java
@@ -2,6 +2,7 @@ package dev.nau.rna_transcription;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,10 @@ public class RnaTranscriptionTest {
     @Test
     public void testEmptyRnaSequence() {
         assertThat(RnaTranscription.toRNA("").isEmpty(), is(true));
+    }
+
+    @Test
+    public void testRnaTranscriptionOfCytosineIsGuanine() {
+        assertThat(RnaTranscription.toRNA("C"), is(equalTo("G")));
     }
 }
