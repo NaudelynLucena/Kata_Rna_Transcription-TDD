4b56abeea0728c4b3d18708b97ecd29eaa3abd70
NaudelynLucena <naulunau@gmail.com>
2025-03-09 22:45:18 +0100

    feat: implement conversion of cytosine ('C') to guanine ('G') Updated the  method to now return 'G' when the input is 'C'. Test passes

--- src/main/java/dev/nau/rna_transcription/RnaTranscription.java
+++ src/main/java/dev/nau/rna_transcription/RnaTranscription.java
@@ -2,6 +2,7 @@ package dev.nau.rna_transcription;
 
 public class RnaTranscription {
     public static String toRNA(String dna) {
+        if (dna.equals("C")) return "G";
         return "";
     }
 
